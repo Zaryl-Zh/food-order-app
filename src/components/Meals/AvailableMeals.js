728de026b887d9ccc7e54197e1a79960db39d399
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-05-04 00:04:58 +0600

    changes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from '../UI/Card'
 import classes from './AvailableMeals.module.css'
 import MealItem from './MealItem'
 
@@ -37,9 +38,12 @@ const AvailableMeals = () => {
 
   return (
     <section className={classes.meals}>
-        <ul>
-           {mealsList}
-        </ul>
+        <Card>
+            <ul>
+              {mealsList}
+            </ul>
+        </Card>
+       
     </section>
   )
 }
